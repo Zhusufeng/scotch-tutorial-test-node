ab12ceb14ded0c35d465ddc0e9eb5d591090d966
Lisa Gee <zhusufeng@gmail.com>
2017-11-09 10:08:10 -0800

    Add db connection and bodyParser to server

--- server.js
+++ server.js
@@ -28,4 +28,19 @@ let options = {
     }
   }
 };
+// db connection
+mongoose.connect(config.DBHost, options);
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection: error:'));
 
+// don't show the log when it is test
+if (config.util.getEnv('NODE_ENV') !== 'test') {
+  // use morgan to log at command line
+  app.use(morgan('combined')); // 'combined' outputs the Apache style LOGs
+}
+
+// parse application/json and look for raw text
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/json' }));
