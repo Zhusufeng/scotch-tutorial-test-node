0a566c8d96c4cd4e94847796c59be0a27f5f630a
Lisa Gee <zhusufeng@gmail.com>
2017-11-09 10:10:28 -0800

    Add endpoints for server

--- server.js
+++ server.js
@@ -44,3 +44,19 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/json' }));
+
+// end points
+app.get('/', (req, res) => res.json({ message: 'Welcome to our Bookstore!' }));
+
+app.route('/book')
+  .get(book.getBooks)
+  .post(book.postBook);
+app.route('/book/:id')
+  .get(book.getBook)
+  .delete(book.deleteBook)
+  .put(book.updateBook);
+
+app.listen(port);
+console.log('Listening on port ' + port);
+
+module.exports = app;
